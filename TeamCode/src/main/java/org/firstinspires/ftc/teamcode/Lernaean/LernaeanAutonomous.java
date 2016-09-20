16018113970ba25d90bcdc8f6b45d39a8b1b4935
Arib Dhuka <atxarib99@gmail.com>
2016-09-20 17:14:23 -0500

    Updated First Auto with rest of functionality and color sensor methods to the AutoMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lernaean/LernaeanAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lernaean/LernaeanAutonomous.java
@@ -8,6 +8,7 @@ public class LernaeanAutonomous extends LernaeanAutoMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+        //Red Team
         map();
 
         moveForward(.5, 180); //move forward off wall
@@ -16,14 +17,45 @@ public class LernaeanAutonomous extends LernaeanAutoMode {
 
         pRotate(.5, -45); //turn towards beacons
 
-        while(!middleLine()){
+        while (!middleLine()) {
             startMotors(.8, .8); //move forward until middle sensor on line
         }
         stopMotors();
 
-        while(!sideLine()){
+        while (!sideLine()) {
             startMotors(-.5, .5); //move backward until aligned with line
         }
         stopMotors();
+
+        //Robot should now be in correct position ready to press beacon
+
+        rightRightSemi();       //push button slightly out to detect color
+
+        if (isRightRed()) {      //if the color is red push and we are red team press the red button
+            rightRightOut();
+            Thread.sleep(500);
+            rightRightIn();
+        } else {                //else push out the other button
+            rightRightIn();
+            rightLeftOut();
+            Thread.sleep(500);
+            rightLeftIn();
+        }
+
+        while (!middleLine()) {  //move forward again to the next beacon
+            startMotors(.8, .8);
+        }
+
+        stopMotors();
+
+        if (isRightRed()) {      //if the color is red push and we are red team press the red button
+            rightRightOut();
+            Thread.sleep(500);
+            rightRightIn();
+        } else {                //else push out the other button
+            rightRightIn();
+            rightLeftOut();
+            Thread.sleep(500);
+        }
     }
 }
