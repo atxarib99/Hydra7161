9971fe4002c49c40599a2f2c5db90916e2944599
TheHunterGames <nicholas.tyler.chan@gmail.com>
2016-09-21 17:30:01 -0500

    nothing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lernaean/LernaeanRedBeaconAuto.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.Lernaean;
+
+/**
+ * Created by TechMaster on 9/21/2016.
+ */
+public class LernaeanRedBeaconAuto extends LernaeanAutoMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        map();
+
+        moveForward(.5, 180); //move forward off wall
+
+        pRotateNoReset(.5, 0); //correct for drift
+
+        pRotate(.5, 45); //turn towards beacons
+
+        while(!middleLine()){
+            startMotors(.8, .8); //move forward until middle sensor on line
+        }
+        stopMotors();
+
+        while(!sideLine()){
+            startMotors(-.5, .5); //pivot backward until aligned with line
+        }
+        stopMotors();
+
+        if (isLeftRed){           //read color sensor, extend appropriate button pusher
+            leftRightOut();
+        } else {
+            leftLeftOut();
+        }
+
+        moveForward(.5, 200); //move forward off of first line
+
+        while(!middleLine()){
+            startMotors(.8, .8); //move forward until middle sensor on line
+        }
+        stopMotors();
+
+        if (isLeftRed){
+            leftRightOut();
+        } else {
+            leftLeftOut();
+        }
+
+    }
+}
