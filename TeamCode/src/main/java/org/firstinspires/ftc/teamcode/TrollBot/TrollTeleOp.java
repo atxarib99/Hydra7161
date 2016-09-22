527307677beb801e2bf92dd12457f8d91b035f6a
TheHunterGames <nicholas.tyler.chan@gmail.com>
2016-09-22 17:20:56 -0500

    Wrote tele-op for troll bot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollBot/TrollTeleOp.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.TrollBot;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by TechMaster on 9/22/2016.
+ */
+public class TrollTeleOp extends TrollOpMode {
+
+    @Override
+        public void loop() {
+            if((Math.abs(gamepad1.right_stick_y) > .05) || Math.abs(gamepad1.left_stick_y) > .05){
+            startMotors(gamepad1.right_stick_y, gamepad1.left_stick_y);
+            } else {
+            stopMotors();
+            }
+
+            getAngles();
+
+            composeTelemetry();
+    }
+}
