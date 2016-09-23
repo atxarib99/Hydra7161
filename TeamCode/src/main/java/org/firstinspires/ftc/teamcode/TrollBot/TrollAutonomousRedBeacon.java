e42bc91fd0e8b8b22633f7242a28dd2c7ee823cd
Arib Dhuka <atxarib99@gmail.com>
2016-09-23 08:11:10 -0500

    finished troll bot teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollBot/TrollAutonomousRedBeacon.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.TrollBot;
+
+/**
+ * Created by Arib on 9/23/2016.
+ */
+public class TrollAutonomousRedBeacon extends TrollAutoMode {
+    @Override
+    public void loop(){
+
+    }
+}
