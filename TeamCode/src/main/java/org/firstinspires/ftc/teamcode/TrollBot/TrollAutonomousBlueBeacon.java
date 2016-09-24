aa013e869b0cd32aa7712f8332192c88581001dc
TheHunterGames <nicholas.tyler.chan@gmail.com>
2016-09-24 18:37:25 -0500

    Finished blue side beacon autonomous for troll bot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollBot/TrollAutonomousBlueBeacon.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.TrollBot;
+
+/**
+ * Created by TechMaster on 9/24/2016.
+ */
+public class TrollAutonomousBlueBeacon extends TrollAutoMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Blue Team
+        map();
+
+        moveForwardPID(.5, 180); //move forward off wall
+
+        pRotateNoReset(.5, 0); //correct for drift
+
+        pRotate(.5, 45); //turn towards beacons
+
+        while (!middleLine()) {
+            startMotors(.8, .8); //move forward until middle sensor on line
+        }
+        stopMotors();
+
+        while (!sideLine()) {
+            startMotors(.5, -.5); //move backward until aligned with line
+        }
+        stopMotors();
+
+        //Robot should now be in correct position ready to press beacon
+        //Color sensor and button pusher code would go here eventually
+
+        while (!middleLine()) {  //move forward again to the next beacon
+            startMotors(.8, .8);
+        }
+
+        //Robot should now be in correct position ready to press beacon
+        //Color sensor and button pusher code would go here eventually
+
+        stopMotors();
+
+    }
+}
