0c92be3788f516040f197010f3eb4a50ef748440
Arib Dhuka <atxarib99@gmail.com>
2017-02-15 18:40:37 -0600

    added a test file to test if all the encoders are running

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lernaean/ModuleTesting/EncoderTest.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Lernaean.ModuleTesting;
+
+import org.firstinspires.ftc.teamcode.Lernaean.LernaeanOpMode;
+
+/**
+ * Created by Arib on 1/19/2017.
+ */
+
+public class EncoderTest extends LernaeanOpMode {
+
+    @Override
+    public void loop() {
+        telemetry.update();
+    }
+}
