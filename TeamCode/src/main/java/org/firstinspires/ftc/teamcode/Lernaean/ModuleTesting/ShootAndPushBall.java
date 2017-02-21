b6f984d481b705588a8709e02c39401770f8c3f6
Arib Dhuka <atxarib99@gmail.com>
2017-02-21 10:10:20 -0600

    renamed files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lernaean/ModuleTesting/ShootAndPushBall.java
@@ -0,0 +1,163 @@
+package org.firstinspires.ftc.teamcode.Lernaean.ModuleTesting;
+
+import android.widget.ThemedSpinnerAdapter;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.teamcode.Libraries.BeaconPushers;
+import org.firstinspires.ftc.teamcode.Libraries.Drivetrain;
+import org.firstinspires.ftc.teamcode.Libraries.Lift;
+import org.firstinspires.ftc.teamcode.Libraries.Manipulator;
+import org.firstinspires.ftc.teamcode.Libraries.Shooter;
+
+/**
+ * Created by Arib on 10/20/2016.
+ */
+@Autonomous(name = "ShooterCapBall", group = "LinearOpMode")
+public class ShootAndPushBall extends LinearOpMode {
+
+    private Drivetrain drivetrain;
+    private Manipulator manipulator;
+    private Shooter shooter;
+    private BeaconPushers beaconPushers;
+    private Lift lift;
+    private double voltage;
+
+    private String version;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drivetrain = new Drivetrain(this);
+        manipulator = new Manipulator(this);
+        shooter = new Shooter(this);
+        beaconPushers = new BeaconPushers(this);
+        lift = new Lift(this);
+        composeTelemetry();
+        Thread.sleep(2000);
+        voltage = hardwareMap.voltageSensor.get("Motor Controller 5").getVoltage();
+
+        version = "1.43";
+
+        telemetry.addData("version: ", version);
+        telemetry.addData("voltage", voltage);
+        telemetry.addData("init", "init fully finished");
+        telemetry.update();
+
+        drivetrain.resetEncoders();
+
+        while(!opModeIsActive()) {
+            telemetry.update();
+            idle();
+        }
+
+        drivetrain.sensor.gyro.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        telemetry.addData("currentStep", "moving off the wall");
+        telemetry.update();
+
+        //soft reset the encoders
+        drivetrain.setNullValue();
+
+        drivetrain.moveForward(.35, 4000, 5000);
+
+        //run a saftey stop command. the previous method has one but this ensures it
+        drivetrain.stopMotors();
+
+        //soft reset the encoders
+        drivetrain.setNullValue();
+
+        //display that we are going to shoot
+        telemetry.addData("currentStep", "shooting");
+        telemetry.update();
+
+        //turn the safe off
+        manipulator.activateShooter();
+
+        lift.openArms();
+
+        Thread.sleep(10);
+
+        lift.topGrab();
+
+        //start the shooter at the calculated power from the voltage value saved
+        shooter.startShooter(-shooter.getNeededPower(voltage));
+
+        //wait one second for the shooter to spin-up
+        Thread.sleep(1000);
+
+        //start moving the collecter
+        manipulator.runCollector(-1);
+
+        //let the shooter run for 3 seconds
+        Thread.sleep(900);
+
+        manipulator.runCollector(0);
+
+        Thread.sleep(400);
+
+        manipulator.runCollector(-1);
+
+        Thread.sleep(1750);
+
+        //display that we are gonna start our rotation
+        telemetry.addData("currentStep", "rotating");
+        telemetry.update();
+
+        //stop the shooter
+        shooter.stopShooter();
+
+        lift.topUngrab();
+
+        lift.grabArms();
+
+        Thread.sleep(10000);
+
+        drivetrain.moveBackward(.2, 5500, 5000);
+
+        drivetrain.stopMotors();
+
+    }
+
+    private void composeTelemetry() {
+        telemetry.addLine()
+                .addData("AVg", new Func<String>() {
+                    @Override public String value() {
+                        return "avg: " + drivetrain.getEncoderAvg();
+                    }
+                });
+        telemetry.addLine()
+                .addData("ods", new Func<String>() {
+                    @Override public String value() {
+                        return "ods: " + drivetrain.sensor.leftODS() + " " + drivetrain.sensor.rightODS();
+                    }
+                });
+        telemetry.addLine()
+                .addData("gyro", new Func<String>() {
+                    @Override public String value() {
+                        return "gyro: " + drivetrain.sensor.getGyroYaw();
+                    }
+                });
+        telemetry.addLine()
+                .addData("motorLPower", new Func<String>() {
+                    @Override public String value() {
+                        return "leftPower: " + drivetrain.motorBL.getPower();
+                    }
+                });
+        telemetry.addLine()
+                .addData("motorRPower", new Func<String>() {
+                    @Override public String value() {
+                        return "rightPower: " + drivetrain.motorBR.getPower();
+                    }
+                });
+        telemetry.addLine()
+                .addData("Color", new Func<String>() {
+                    @Override public String value() {
+                        return "Color: " + beaconPushers.getColorVal();
+                    }
+                });
+    }
+}
